94f351a2092a68b2d22dcd7ca22370cb001c9dde
Lilzk22 <zombiekiller199914@gmail.com>
2021-12-14 13:25:12 -0600

    December 14th
    -Perk System Update
    -New Zombie Animations
    -New weapons pack

--- Assets/Scripts/PlayerScipts/PerkPopUp.cs
+++ Assets/Scripts/PlayerScipts/PerkPopUp.cs
@@ -6,35 +6,46 @@ using UnityEngine.UI;
 public class PerkPopUp : MonoBehaviour
 {
     public GameObject perkUi;
+    public GameObject perkTrigger;
     public float perkPrice;
-    public bool hasPerk;
+    public bool hasPerk, isInBounds, isBuyable;
+
     PlayerPoints playerPoints;
     public void Start()
     {
         playerPoints = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPoints>();
-
         perkUi.SetActive(false);
+        isBuyable = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" && !hasPerk)
+        if (other.tag == "Player" && !hasPerk && isBuyable)
         {
             perkUi.SetActive(true);
-
-            if (playerPoints.currScore >= perkPrice && Input.GetKey(KeyCode.E))
-            {
-                playerPoints.currScore -= perkPrice;
-                hasPerk = true;
-                perkUi.SetActive(false);
-            }
+            isInBounds = true;    
         }
     }
+
+    private void Update()
+    {
+        if (isInBounds && Input.GetKeyDown(KeyCode.F) && playerPoints.currScore >= perkPrice)
+        {
+            playerPoints.currScore -= perkPrice;
+            hasPerk = true;
+            isBuyable = false;
+            isInBounds = false;
+            perkUi.SetActive(false);
+            perkTrigger.SetActive(false);
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
-        if(other.tag == "Player" && !hasPerk)
+        if (other.tag == "Player" && !hasPerk)
         {
             perkUi.SetActive(false);
+            isInBounds = false;
         }
     }
 }
--- Assets/Scripts/PlayerScipts/PlayerStats.cs
+++ Assets/Scripts/PlayerScipts/PlayerStats.cs
@@ -15,8 +15,6 @@ public class PlayerStats : CharacterStats
 
         maxStamina = 100;
         currStamina = maxStamina;
-
-        SetStats();
     }
 
     public override void Die()
@@ -24,20 +22,13 @@ public class PlayerStats : CharacterStats
         Debug.Log("DEAD");
     }
 
-    void SetStats()
-    {
-        playerUI.healthAmount.text = currentHealth.ToString("0");
-        playerUI.staminaAmount.text = currStamina.ToString("0");
-    }
     public override void CheckHealth()
     {
         base.CheckHealth();
-        SetStats();
     }
 
     public override void CheckStamina()
     {
         base.CheckStamina();
-        SetStats();
     }
 }
